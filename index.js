df6fc952a200bc50c07d69b657d11ee6b2552ca8
MrLumus <39524743+MrLumus@users.noreply.github.com>
2022-10-11 03:07:38 +0300

    refactored

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // IMPORTS
 const TelegramApi = require("node-telegram-bot-api");
 
-const { gameOptions, againOptions } = require("./options");
+const { commandOptions, gameOptions, againOptions } = require("./options");
 const sequelize = require("./db");
 const UserModel = require("./models");
 
@@ -10,12 +10,24 @@ const token = "5601386337:AAFsYcpxif5Hhed_zx-Y7oMFqh-l1FkhQEo";
 const bot = new TelegramApi(token, {polling: true});
 const chats = {};
 
-// Функция-обработичк начала игры
+// Функция-обработичк начала игры (команда /game)
 const startGame = async (chatId) => {
   await bot.sendMessage(chatId, "Сейчас я загадаю цифру от 0 до 9, а ты должен(на) отгадать ее!");
   const randomNumber = Math.floor(Math.random() * 10);
   chats[chatId] = randomNumber;
   return bot.sendMessage(chatId, "Отгадывай!", gameOptions);
+};
+// Функция-обработичк для получения инфо об игроке (команда /info)
+const getInfo = async (chatId, from) => {
+  const user = await UserModel.findOne({chatId});
+  return bot.sendMessage(chatId, `Тебя зовут - ${from.first_name} ${from.last_name}.\nПравильных ответов - ${user.right}\nНеправильных ответов - ${user.wrong}`, commandOptions);
+};
+// Функция-обработичк на команду /start
+const onStart = async (chatId, from) => {
+  if (!UserModel.findOne({chatId})){
+    await UserModel.create({chatId});
+  }
+  await bot.sendMessage(chatId, `Привет, ${from.username}! Ты готов(а)?`, commandOptions);
 }
 
 // Функция-обработчик запуска диалога с ботом
@@ -30,60 +42,67 @@ const start = async () => {
   }
 
   //Установка команд для бота
-  bot.setMyCommands([
-    {command: "/start", description: "Приветсвие"},
-    {command: "/info", description: "Информация о пользователе"},
-    {command: "/game", description: "Игра 'Угадай число'"},
-  ]);
+  bot.setMyCommands(commandOptions);
 
   // Обработка входящих боту сообщений
   bot.on("message", async msg => {
+
+    // Получение информации о пользователе
     const text = msg.text;
     const chatId = msg.chat.id;
     const from = msg.from;
     
     try {
+      await bot.sendSticker(chatId, "https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/11.webp")
       // Проверка сообщений на команды
       switch(text){
         case "/start": {
-          if (!UserModel.findOne({chatId})){
-            await UserModel.create({chatId});
-          }
-          await bot.sendSticker(chatId, "https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/11.webp")
-          await bot.sendMessage(chatId, `Привет, ${from.username}! Ты готов(а)?`);
+          return onStart(chatId, from);
         };
         case "/info": {
-          const user = await UserModel.findOne({chatId});
-          return bot.sendMessage(chatId, `Тебя зовут: ${from.first_name} ${from.last_name}. Правильных ответов: ${user.right}, неправильных ответов: ${user.wrong}`);
+          return getInfo(chatId, from);
         };
         case "/game": {
-          startGame(chatId);
+          return startGame(chatId);
         };
         default: 
-          return bot.sendMessage(chatId, "Извини, я тебя не понимаю, попробуй еще раз");
+          return bot.sendMessage(chatId, "Извини, я тебя не понимаю, попробуй еще раз", commandOptions);
       }
     }
     catch (e) {
-      return bot.sendMessage(chatId, `Произошла какая-то ошибка :(\nУже исправляю`)
+      return bot.sendMessage(chatId, `Произошла какая-то ошибка :(\nУже исправляю`, commandOptions)
     }
   });
 
   // Обработка нажатия кнопок-действий
   bot.on("callback_query", async msg => {
+
+    //Получение информации о пользователе
     const data = msg.data;
     const chatId = msg.message.chat.id;
+    const from = msg.from;
 
+    //Получение информации о пользователе из БД
     const user = await UserModel.findOne({chatId});
 
     // Проверка кнопок на команды и цифры
     switch (data){
-      case "/again": {
+      case "/start": {
+        return onStart(chatId, from);
+      }
+      case "/info": {
+        return getInfo(chatId, from);
+      }
+      case "/game": {
         return startGame(chatId);
       }
       case String(chats[chatId]): {
         user.right += 1;
         await bot.sendMessage(chatId, "Поздравляю, ты отгадал(а) цифру!", againOptions);
         break;
+      }
+      case "/again": {
+        return startGame(chatId);
       }
       default: {
         user.wrong += 1;
--- options.js
+++ options.js
@@ -1,4 +1,13 @@
 module.exports = {
+  commandOptions: {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{text: "Приветствие", callback_data: "/start"}],
+        [{text: "Ваш счёт", callback_data: "/info"}],
+        [{text: "Начать игру", callback_data: "/game"}],
+      ],
+    }),
+  },
   gameOptions: {
     reply_markup: JSON.stringify({
       inline_keyboard: [
@@ -6,6 +15,7 @@ module.exports = {
         [{text: "4", callback_data: "4"}, {text: "5", callback_data: "5"}, {text: "6", callback_data: "6"}],
         [{text: "7", callback_data: "7"}, {text: "8", callback_data: "8"}, {text: "9", callback_data: "9"}],
         [{text: "0", callback_data: "0"}],
+        [{text: "Вернуться", callback_data: "/start"}],
       ],
     }),
   },
@@ -13,6 +23,7 @@ module.exports = {
     reply_markup: JSON.stringify({
       inline_keyboard: [
         [{text: "Играть еще раз", callback_data: "/again"}],
+        [{text: "Ваш счёт", callback_data: "/info"}],
       ],
     }),
   },
